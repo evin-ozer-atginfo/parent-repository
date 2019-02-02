ded4f9e4e7033a816ef9f1bf52b6cdd6c8c5b1ae
evin-ozer-atginfo <evin.ozer@atginfo.com>
2019-02-02 11:30:51 -0700

    Another update 2

--- subclass.py
+++ subclass.py
@@ -12,7 +12,7 @@ from base import Base
 class Subclass(Base):
 	def __init__(self, name):
 		super().__init__()
-		self.name = name + 'Meowss'
+		self.name = name + 'Meowsss'
 
 if __name__ == '__main__':
 	s = Subclass('Me')
