18085ec52318c72ec3137495fa024d35232af4c0
evin-ozer-atginfo <evin.ozer@atginfo.com>
2019-02-02 10:40:11 -0700

    Updated python module to be more appropriately named as sub-class.

---
+++ subclass.py
@@ -0,0 +1,20 @@
+#+ Framework libraries.
+import os
+import sys
+__real_path = os.path.split(os.path.realpath(__file__))[0]
+lib_paths = [
+    os.path.abspath(os.path.join(__real_path, './child-repository'))
+]
+sys.path += lib_paths
+
+from base import Base
+
+class Subclass(Base):
+	def __init__(self, name):
+		super().__init__()
+		self.name = name
+
+if __name__ == '__main__':
+	s = Subclass('Me')
+	print(s.name)
+	print(s.text)
